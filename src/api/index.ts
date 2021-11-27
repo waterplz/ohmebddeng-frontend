b47ce9b9c12dcb81323f5c79cb68fb3f853888e0
Peter(Eunsu) Kim <peter.kim@mathpresso.com>
2021-11-27 17:59:51 +0900

    Feature/favicon (#38)
    * feat: add favicon and edit base url
    
    * chore: edit environment variable name

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const baseURL = 'https://api.ohmebddeng.kr/v1';
+export const baseURL =
+  process.env.NEXT_PUBLIC_APP_ENV === 'prod'
+    ? 'https://api.ohmebddeng.kr/v1'
+    : 'https://api-dev.ohmebddeng.kr/v1';
 
 export const apiClient = axios.create({
   baseURL,
